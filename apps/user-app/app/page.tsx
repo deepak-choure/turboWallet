18aafcea5b9265676544236f6780b8472a073802
Deepak Choure <dchoure2306@gmail.com>
2024-10-05 17:52:21 +0530

    Revert "Merge pull request #1 from deepak-choure/head"
    This reverts commit 4d6cea731cbe5fde5d767d8b15873a5d907d5009, reversing
    changes made to 600377ba884bae20bdd307d97557ca89c9a9b20b.

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,8 +4,7 @@ import { redirect } from "next/navigation";
 
 export default async  function Home() {
   const session = await getServerSession(authOptions);
-  // console.log(session.user)
-
+  console.log(session.user)
   if(session.user){
     redirect("/dashboard")
   }
