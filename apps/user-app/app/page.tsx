ac05768660452dc4cb7189a18b4870f52ee3b472
Deepak Choure <dchoure2306@gmail.com>
2024-10-05 18:28:41 +0530

    fix:the head changes got reverted
    This reverts commit 18aafcea5b9265676544236f6780b8472a073802.

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,7 +4,8 @@ import { redirect } from "next/navigation";
 
 export default async  function Home() {
   const session = await getServerSession(authOptions);
-  console.log(session.user)
+  // console.log(session.user)
+
   if(session.user){
     redirect("/dashboard")
   }
